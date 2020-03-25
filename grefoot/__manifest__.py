a30b43f808019f03a7090367b2f4ceedfa223460
malak duraidi <malakduraidi@malaks-MacBook-Pro.local>
2020-03-25 22:01:57 +0200

    products list

--- grefoot/__manifest__.py
+++ grefoot/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','product','ks_theme_kinetik'],
+    'depends': ['base','product','ks_theme_kinetik','checkout'],
 
     # always loaded
     'data': [
--- grefoot/controllers/controllers.py
+++ grefoot/controllers/controllers.py
@@ -9,13 +9,30 @@ class Grefoot(http.Controller):
     def index(self, **kw):
         return "I am a live"
 
-    @http.route('/grefoot/api/products/',method=['POST'],auth='public',type="http")
-    def list(self, **kw):
-        results=http.request.env['product.product'].sudo().search_read([],fields=['name','website_url','product_tmpl_id'])
+    @http.route('/grefoot/api/products/',method=['POST'],auth='public',type='json')
+    def product_list(self, **kw):
+
+        fields = ""
+        model = ""
+        
+        if 'model' in http.request.params:
+            model = http.request.params['model'] 
+        if 'fields' in http.request.params:
+            fields = http.request.params['fields']
 
         headers = {'Content-Type': 'application/json'}
-        body={'results':{'code':200,'message':results}}
-        return Response(json.dumps(body),status=200,headers=headers)
+
+        records = http.request.env[model].sudo().search_read([],fields=fields)
+        if 'product_tmpl_id' in fields:
+            for rec in records:
+                rec['product_tmpl_id'] = rec['product_tmpl_id'][0]
+
+        if fields and model:
+            pass
+        else:
+            return {'code':404, 'records':records}
+
+        return {'code':200,'records':records}
 
 
     @http.route('/grefoot/api/sitesetting/',auth='public',type="http")
